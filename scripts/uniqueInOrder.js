500ae1f6bc6323eef71ba1f74fbbb5aaf4352b2f
Yaroslav Potapenko <potapenkoyaroslav.o@gmail.com>
2020-05-05 15:24:24 +0300

    added sum of big integer function

--- scripts/uniqueInOrder.js
+++ scripts/uniqueInOrder.js
@@ -9,7 +9,7 @@
 // uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 // uniqueInOrder([1,2,2,3,3])       == [1,2,3]
 
-var uniqueInOrder = function(iterable){
+let uniqueInOrder = function(iterable){
 
     let sequence;
     let result = [];
